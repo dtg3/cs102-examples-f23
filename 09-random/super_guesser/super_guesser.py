78c97ef2e8c6d090026a28d8f09d445b9ef6c900
Drew Guarnera <dguarnera@wooster.edu>
2023-10-11 08:46:20 -0400

    Fix bug in super_guesser

--- 09-random/super_guesser/super_guesser.py
+++ 09-random/super_guesser/super_guesser.py
@@ -9,7 +9,7 @@ def validate_input(user_input):
 
     # Makes the game boring since the range
     #   1-0 doesn't make sense and 1-1 is boring...
-    if number <= 1:
+    if number < 1:
         return None
     
     return number
